580e7f136565542ea7550d358829ea0772db424e
Daniel Saewitz <switz213@gmail.com>
2018-09-19 22:43:04 -0400

    Updated dotfiles

--- copy.sh
+++ copy.sh
@@ -1,11 +1,9 @@
 cp ~/.vimrc .
 cp ~/.config/nvim/init.vim
 git diff
-read -r -p "Are you sure? [Y/n]" response
-response=${response,,} # tolower
+read -r -p "Are you sure? [y/n]" response
 if [[ $response =~ ^(yes|y| ) ]] || [[ -z $response ]]; then
-  your-action-here
+  git add .
+  git commit -m "Updated dotfiles"
+  git push origin HEAD
 fi
-git add .
-git commit -m "Updated dotfiles"
-git push origin HEAD

98d872149590a7e136956d4dcd95b217c075c8a1
Daniel Saewitz <switz213@gmail.com>
2018-09-20 01:13:17 -0400

    Updated dotfiles

--- copy.sh
+++ copy.sh
@@ -1,5 +1,7 @@
 cp ~/.vimrc .
+cp ~/.tmux.conf .
 cp ~/.config/nvim/init.vim .config/nvim
+
 git diff
 read -r -p "Are you sure? [y/n]" response
 if [[ $response =~ ^(yes|y| ) ]] || [[ -z $response ]]; then

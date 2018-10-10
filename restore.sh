b5a4b80f458f4c170c3c719bd9fa0747c71e44c4
Daniel Saewitz <daniel.saewitz@inturn.co>
2018-10-10 10:33:46 -0400

    Updated dotfiles

--- restore.sh
+++ restore.sh
@@ -4,6 +4,7 @@ if [[ $response =~ ^(yes|y| ) ]] || [[ -z $response ]]; then
   cp .vimrc ~
   cp .tmux.conf ~
   mkdir -p ~/.config/nvim
+  mkdir -p ~/.config/fish/functions
   cp .config/nvim/init.vim ~/.config/nvim
   cp .config/fish/functions/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
 fi
